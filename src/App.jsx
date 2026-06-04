7488d3845dee2f34b35f99948ead09a2f2b61a78
Nisarg-jain <jainnisag98@gmail.com>
2026-06-04 20:31:53 +0530

    Created user context and user context provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline bg-green-500">
-        Hello world!
-      </h1>
-    </div>
+    <h1>hello </h1>
   )
 }
 
